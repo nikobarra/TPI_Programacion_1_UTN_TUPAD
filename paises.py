7f519bd3a1575eb1be45b88ae3339f02e9f82264
Nicolas M. Barra Pelecano <nicolasbarrapelecano@gmail.com>
2026-06-08 14:49:58 -0300

    crar constantes para color ansi

---
+++ paises.py
@@ -0,0 +1,15 @@
+import os
+import csv
+import sys
+
+# Constantes de colores ANSI para la interfaz
+ROJO = "\033[91m"
+VERDE = "\033[92m"
+AMARILLO = "\033[93m"
+AZUL = "\033[94m"
+MAGENTA = "\033[95m"
+CIAN = "\033[96m"
+BLANCO = "\033[97m"
+NEGRITA = "\033[1m"
+RESET = "\033[0m"
+
