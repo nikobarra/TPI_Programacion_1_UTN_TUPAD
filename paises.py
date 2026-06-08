d80f23482f4a86f2e60576ba7f0f6bba3955e36c
Nicolas M. Barra Pelecano <nicolasbarrapelecano@gmail.com>
2026-06-08 17:25:43 -0300

    implementar función principal main y punto de entrada del script y estructura de funciones

--- paises.py
+++ paises.py
@@ -115,3 +115,66 @@ def guardar_csv(nombre_archivo, lista_paises):
     except Exception as e:
         imprimir_cuadro_error(f"No se pudo guardar en el archivo: {e}")
         return False
+
+
+def mostrar_todos (paises):
+    pass 
+
+def agregar_pais(paises):
+    pass 
+
+def actualizar_pais(paises):
+    pass 
+
+def buscar_pais (paises):
+    pass 
+
+def filtrar_paises (paises):
+    pass 
+
+def ordenar_paises (paises):
+    pass 
+
+def mostrar_estadisticas (paises):
+    pass 
+
+def main():
+    limpiar_pantalla()
+    imprimir_titulo("BIENVENIDO AL SISTEMA DE GESTIÓN MUNDIAL")
+    
+    lista_paises = cargar_csv("paises.csv")
+    
+    if not lista_paises:
+        input(f"\n{AMARILLO}Presione Enter para continuar con el programa vacío...{RESET}")
+
+    while True:
+        limpiar_pantalla()
+        imprimir_menu()
+        
+        opcion = input(f"\n{AMARILLO}Seleccione una opción (0-7): {RESET}")
+        
+        if opcion == "1":
+            agregar_pais(lista_paises)
+        elif opcion == "2":
+            actualizar_pais(lista_paises)
+        elif opcion == "3":
+            buscar_pais(lista_paises)
+        elif opcion == "4":
+            filtrar_paises(lista_paises)
+        elif opcion == "5":
+            ordenar_paises(lista_paises)
+        elif opcion == "6":
+            mostrar_estadisticas(lista_paises)
+        elif opcion == "7":
+            imprimir_titulo("LISTADO COMPLETO DE PAÍSES")
+            mostrar_todos(lista_paises)
+        elif opcion == "0":
+            imprimir_cuadro_info("Gracias por utilizar el sistema. ¡Hasta pronto!")
+            sys.exit()
+        else:
+            imprimir_cuadro_advertencia("Opción no válida. Intente nuevamente.")
+        
+        input(f"\n{BLANCO}Presione Enter para volver al menú...{RESET}")
+
+if __name__ == "__main__":
+    main()
